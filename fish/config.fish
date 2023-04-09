c6e366b9c0f0dee4dea1333bffbef28bc2b90157
MartinTscholl <94525904+MartinTscholl@users.noreply.github.com>
2023-04-10 00:58:30 +0200

    update

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,9 @@ alias backupctl="bash ~/git/repos/shellscripts/backupctl/backupctl.sh"
 alias gitctl="bash ~/git/repos/shellscripts/gitctl/gitctl.sh"
 
 # custom commands
-alias usb="cd /run/media/martin/"
+alias usb="cd /run/media/martin/ && cd 4BE7-5B2E"
+alias eusb="umount /run/media/martin/4BE7-5B2E"
+alias eallusb="umount /run/media/martin/*"
 alias ngs="nvm use 14.17.1 && ng serve"
 alias nvv="nvm use 14.17.1"
 alias nzip="zip -x '*node_modules*' -r"
